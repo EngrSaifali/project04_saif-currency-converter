39c35a1e330596efac3e871de15c01f28782eaf1
EngrSaifali <saifaliapril2004@gmail.com>
2024-04-16 19:52:04 +0500

    Currency Converter using Typescript

---
+++ main.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+// Currency Converter using Typescript
+import inquirer from "inquirer";
+let currency = {
+    usd: 1, // Base Currency
+    pkr: 278,
+    euro: 0.94,
+    ary: 32.49
+};
+let CurrConverter = await inquirer.prompt([
+    {
+        name: "fromCurrency",
+        type: "list",
+        choices: ["usd", "pkr", "euro", "ary"],
+        message: "Enter a currency from which you want to convert."
+    },
+    {
+        name: "toCurrency",
+        type: "list",
+        choices: ["usd", "pkr", "euro", "ary"],
+        message: "Enter a currency in which you want to get converted."
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter an amount :"
+    }
+]);
+let userFromCurrency = currency[CurrConverter.fromCurrency];
+let userToCurrency = currency[CurrConverter.toCurrency];
+let amount = CurrConverter.amount;
+let baseAmount = amount / userFromCurrency;
+let convertAmount = baseAmount * userToCurrency;
+console.log(convertAmount);
